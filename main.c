816e2da667aa2b4708ab52e426f4492beaeab803
Lennon Donaghy <donaghylennon@gmail.com>
2021-07-25 23:01:55 +0100

    Added the ability to get filename automatically
    The filename is retrieved from the url if the filename argument is
    omitted.

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <curl/curl.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
+
+const char *filename_from_url(const char *url);
 
 typedef struct {
     const char *url;
@@ -19,7 +23,7 @@ size_t write_to_file(char *ptr, size_t size, size_t nmemb, void *userdata) {
 }
 
 int main(int argc, char *argv[]) {
-    if(argc < 3) {
+    if(argc < 2) {
         printf("Usage:\n\t%s <url> <filename>\n\n", argv[0]);
         exit(1);
     }
@@ -27,13 +31,19 @@ int main(int argc, char *argv[]) {
     CURL *curl;
     CURLcode res;
     FILE *writefile;
+    const char *writefile_name;
 
-    writefile = fopen(argv[2], "wx");
+    if(argc == 2)
+        writefile_name = filename_from_url(argv[1]);
+    else
+        writefile_name = argv[2];
+
+    writefile = fopen(writefile_name, "wx");
     if(errno == EEXIST) {
-        fprintf(stderr, "Error: Could not open %s -- Already exists\n", argv[2]);
+        fprintf(stderr, "Error: Could not open %s -- Already exists\n", writefile_name);
         exit(1);
     } else if(!writefile) {
-        fprintf(stderr, "Error: Could not open %s\n", argv[2]);
+        fprintf(stderr, "Error: Could not open %s\n", writefile_name);
         exit(1);
     }
     
@@ -56,4 +66,12 @@ int main(int argc, char *argv[]) {
 
     curl_easy_cleanup(curl);
     curl_global_cleanup();
+}
+
+const char *filename_from_url(const char *url) {
+    int i = strlen(url);
+    while(url[i] != '/') {
+        i--;
+    }
+    return &url[i+1];
 }
